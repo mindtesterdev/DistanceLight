9542f2e1d0a3314aa36304ae3a05bea3e7ff50a9
unknown <Jason@PhenomX6.(none)>
2015-03-14 21:49:03 -0400

    Initial Commit after code cleanup and added top comments

--- Distance_Light/Distance_Light.ino
+++ Distance_Light/Distance_Light.ino
@@ -1,16 +1,28 @@
 /*
- HC-SR04 Ping distance sensor]
- VCC to arduino 5v GND to arduino GND
- Echo to Arduino pin 13 Trig to Arduino pin 12
- Red POS to Arduino pin 11
- Green POS to Arduino pin 10
- 560 ohm resistor to both LED NEG and GRD power rail
- More info at: http://goo.gl/kJ8Gl
- Original code improvements to the Ping sketch sourced from Trollmaker.com
- Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
- */
+
+  This is the code to my ultrasonic parking guide (ie a glorified tennisball on a string).
+  Parts are based on the Ping sensor example. 
+  
+   - Expects HC-SR04 Ranging Detector Mod Distance Sensor Trigger and Echo pins connected to pins 3 and 6.
+   - Expects one standard RGB LED (the red green and blue #defines are for those pins on the LED.)
+   - Expects one momentary normally open button that throws HIGH to buttonPin when pressed.
+   
+   LED will be green at >target distance + 150CM (5 Ft.)
+   LED gradients yellow to red from Target Distance+150CM to Target Distance
+   At target distance LED flashes red 5 times then goes to solid red.
+   Pressing the button and holding for 3 seconds will save the currently measured distance as the target distance.
+   The LED will turn blue while you're holding it, then flash green 3 times when the setting is saved to the first 4 bytes of the EEPROM.
+   Continuing to hold the button for another 3 seconds will cause the LED to turn red and the EEPROM will be written out all zeros.
+   If you put this on a hackduino / fresh ATMEGA chip with a programmer you may need to clear EEPROM with above procedure to get the sensor to behave properly (I had some strange results with a new chip).
+  
+  The serial is only enabled for debugging, you could remove all of that if you weren't going to make any changes to make the compiled sketch smaller.
+  
+  Distributed under GPLv3
+  
+*/
 
 #include <EEPROM.h>
+
 #define trigPin 6
 #define buttonPin 2
 #define echoPin 3
@@ -56,6 +68,7 @@ long getDistance()
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
+  
   if(distance <= RANGE & 
     distance > 0)
   {
@@ -63,11 +76,13 @@ long getDistance()
     distances[readIndex] = distance;
     total= total+distances[readIndex];
     readIndex = readIndex+1;
+    
     if(readIndex >= numReadings)
       readIndex = 0;
-   
+      
     average = getCleanAverage(); //total/numReadings;
     int d = average - lastAverage;
+    
     if(abs(d) >= 5)
     {
       lastAverage = average;
@@ -93,44 +108,31 @@ void loop() {
 
   long distance = getDistance();
 
-
   if(distance <= RANGE & 
     distance > 0 &
     digitalRead(buttonPin) == LOW)
-  {
-
-
-    if((millis()-lastReading) >10000)
     {
-      writeColor(0,0,0);
-      delay(50);
-    }
-    else if(average >= target+150)
-    {
-      writeColor(0, 254,0);          
-    }
-    else if(average < target+150 
-      & average > target
-      )
-    {
+      if((millis()-lastReading) >10000)
+      {
+        writeColor(0,0,0);
+        delay(50);
+      }
+      else if(average >= target+150)
+      {
+        writeColor(0, 254,0);          
+      }
+      else if(average < target+150 
+        & average > target
+        )
+      {
+        
       flashes = 0;
       int D = average-target;
-      int Top = target+150;
-      
+      int Top = target+150;      
       int R = map(average, Top, target, 50, 100);
-      int G = map(average, target, Top, 100, 254);
-  
-    /* Serial.print(average);
-      Serial.print("-");
-      Serial.print(target);
-      Serial.print("=");
-      Serial.print(D);
-      Serial.print(", ");
-      Serial.print(R);
-      Serial.print(", ");
-      Serial.println(G);*/
+      int G = map(average, target, Top, 100, 254);    
       writeColor(R,G,0);     
-      //writeColor(150,254,0);
+      
     }
     else if (average <=target)
     {
@@ -144,18 +146,13 @@ void loop() {
       }
       else 
         writeColor(254, 0, 0);
-    }    
-
-
-   
-    
+    }       
   }
   else if(digitalRead(buttonPin) == HIGH)
   {
     setTarget();
   }
-
-
+  
   delay(20);
 }
 
@@ -164,7 +161,6 @@ long getCleanAverage()
   long RetVal = 0;
   long V[numReadings];
   unsigned long T = 0;
-
   unsigned long mean; 
   unsigned long stdDev = 0;
   int sig = 0;
@@ -178,11 +174,13 @@ long getCleanAverage()
     }
   }
   mean = T/sig;
+  
   for (int i = 0; i <numReadings; i++)
   {
     V[i] = distances[i] - mean;
     V[i] = V[i]*V[i];
   }
+  
   T = 0;
 
   for (int i = 0; i <sig; i++)
@@ -190,40 +188,25 @@ long getCleanAverage()
     T+= V[i];
   }
   
-   //Serial.print("T: ");
- // Serial.println(T);  
-
   stdDev = T /sig;
-//  Serial.print("StdDev^2: ");
-//  Serial.println(stdDev);
   stdDev = sqrt(stdDev);
-//  Serial.print("StdDev: ");
-//  Serial.println(stdDev);
   sig=0;
   T = 0;
+  
   for (int i = 0; i<numReadings; i++)
-  {
-    // Serial.print(distances[i]);
-    // Serial.print("/");
-    // Serial.print(mean);
-    // Serial.print( "/");
-    // Serial.println(stdDev);
-    
+  {   
      if(distances[i] <= mean + stdDev & distances[i] >= mean-stdDev)
-    {
-      sig++;
-      T+= distances[i];
-    }
+        {
+          sig++;
+          T+= distances[i];
+        }
   }  
  
   RetVal = T/sig;
- /* Serial.print("Mean: ");
-  Serial.print(mean);
-  Serial.print(" / Corrected: ");
-  Serial.println(RetVal);
-  */
+  
   if(RetVal == -1)
-    RetVal = average;
+    RetVal = average; //If we fail the calculation, just return the existing average to keep everything smoothed.
+    
   return RetVal;
 }
 
@@ -287,11 +270,6 @@ void writeColor(int Red, int Green, int Blue)
   analogWrite(red, Red);
   analogWrite(green, Green);
   analogWrite(blue, Blue);
-  /* Serial.print(Red);
-   Serial.print(", ");
-   Serial.print(Green);    
-   Serial.print(", ");
-   Serial.println(Blue);*/
 }
 
 
